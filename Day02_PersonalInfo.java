1fba6102ef53f5f2869c40f1dd512acfa308fa21
Om Misalkar <YOUR_GITHUB_EMAIL>
2026-07-22 17:47:26 +0530

    Day 2: Print Personal Information

---
+++ Day02_PersonalInfo.java
@@ -0,0 +1,9 @@
+public class Day02_PersonalInfo {
+    public static void main(String[] args) {
+        System.out.println("Name    : Om Misalkar");
+        System.out.println("Course  : MCA");
+        System.out.println("College : Sinhgad Institute of Management");
+        System.out.println("City    : Pune");
+        System.out.println("Country : India");
+    }
+}
