573cdf36b11b8c35d1f99962819e016a1f9687cb
Om Misalkar <YOUR_GITHUB_EMAIL>
2026-07-25 18:43:29 +0530

    Day 3: Add Two Numbers

---
+++ Day03_AddTwoNumbers.java
@@ -0,0 +1,15 @@
+// Day 3 - Add Two Numbers
+
+public class Day03_AddTwoNumbers {
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 15;
+
+        int sum = num1 + num2;
+
+        System.out.println("First Number  : " + num1);
+        System.out.println("Second Number : " + num2);
+        System.out.println("Sum           : " + sum);
+    }
+}
